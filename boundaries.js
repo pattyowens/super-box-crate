1326b58bbc7d07584d0d51d9ec1303b03e7802e0
pattyowens <powens457@gmail.com>
2024-04-05 00:06:06 -0500

    Started building out map

--- boundaries.js
+++ boundaries.js
@@ -4,15 +4,36 @@
 
 // A platform
 /**
- * @typedef PlatformProperties
  * @type {object}
  */
 export class Platform {
     /**
-     * @param {PlatformProperties} params
+     * 
+     * @param {number} x
+     * @param {number} y     
+     * @param {number} width
+     * @param {number} height
+     * @param {string} color
      */
-    constructor(params = {}) {
+    constructor(x, y, width = 50, height = 10, color = "black" ) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+ 
+    /**
+     * Draw the platform 
+     * @param {CanvasRenderingContext2D} context
+     */
+    draw(context) {
+        context.save();
+        context.translate(this.x, this.y);
+        context.fillStyle = this.color;
+        context.fillRect(0, 0, this.width, this.height);
 
+        context.restore();
     }
 
 }
--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 /*jshint esversion: 6 */
 // @ts-check
 
+import { Platform } from "./boundaries.js";
+
+
 /**
  * Set up the UI
  */
 let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas1"));
-let context = canvas.getContext("2d");
+let context = /** @type {CanvasRenderingContext2D} */ canvas.getContext("2d");
 
+// Define our initial map
+let width = canvas.width;
+let height = canvas.height;
+let plat_points = [
+    [0.25 * width, 0.20 * height, width / 2],    // Upper central
+    [0.10 * width, 0.35 * height, width / 4],    // mid1 left
+    [0.65 * width, 0.35 * height, width / 4],    // mid1 right
+    [0.25 * width, 0.50 * height, width / 2],    // mid2 central
+    [0.10 * width, 0.65 * height, width / 4],    // mid1 left
+    [0.65 * width, 0.65 * height, width / 4],    // mid1 right
+    [0.00 * width, 0.80 * height, width / 5],    // mid1 left
+    [0.80 * width, 0.80 * height, width / 5],    // mid1 right
+    [0.25 * width, 0.80 * height, width / 2],    // Bottom central
+    [0.00 * width, 0.00 * height, 10, height],   // Left wall
+    [1.00 * width - 10, 0.00 * height, 10, height]    // Right wall
+];
 
-function draw() {
+let platforms = /** @type {[Platform]} */ [];
 
+plat_points.forEach(function (p) {
+    platforms.push(new Platform(p[0], p[1], p[2], p[3]));
+})
+
+function drawMap() {
+    platforms.forEach(function (platform) {
+        platform.draw(context);
+    })
+}
+
+function drawAll() {
+    drawMap();
 }
 
 let lastTime; // undefined by default
@@ -25,7 +56,7 @@ function loop(timestamp) {
 
 
     // Now we can render
-    draw();
+    drawAll();
     // and loop
     window.requestAnimationFrame(loop);
 }
